21a04b47376d8a78e385e29d460b22bce88e0418
b-juu <erichassett4@gmail.com>
2023-09-30 12:32:52 -0400

    initial commit

--- OperatorExercise/Program.cs
+++ OperatorExercise/Program.cs
@@ -4,7 +4,25 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int a = 17;
+            int b = 4;
+
+            int modulus = a % b;
+            int add = a + b;
+            int subtract = a - b;
+            int divide = a / b;
+            int multiply = a * b;
+
+
+            int quotient = a / b;
+            int remainder = a % b;
+
+            if (a == 17 && b == 4)
+            {
+                Console.WriteLine($"{a}/{b} is {quotient} remainder {remainder}");
+
+
+            }
         }
     }
 }
